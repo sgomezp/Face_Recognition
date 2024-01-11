df912679a31180cce7f9ee9e6caea93c5823564c
sgomezp <sgomezp@gmail.com>
2024-01-11 14:40:51 +0100

    Feat: Ask for name and save the photo but no reset the window

--- pages/Registro_invitado.py
+++ pages/Registro_invitado.py
@@ -3,35 +3,49 @@ import numpy as np
 import os
 import cv2
 
-
 faces_folder = "./faces"
 
 st.title("Registro en el sistema")
 
 st.write("Para poder acceder al sistema, necesitamos que te registres. Para ello, necesitamos que nos des una foto de tu rostro")
 
+# Estado de la aplicación
+state = st.session_state
+if not hasattr(state, 'photo_form_submitted'):
+    state.photo_form_submitted = False
+if not hasattr(state, 'name_form_submitted'):
+    state.name_form_submitted = False
+
 picture = st.camera_input("Toma una foto de tu rostro")
-# Guardar la captura en la carpeta temporal
-
-
 
 if picture:
     st.image(picture, caption="Tu foto", use_column_width=True)
-    foto_ok = st.radio("¿Quieres guardar esta foto?", ("Sí", "No"), index=None, key=None)
-    if foto_ok == "Sí":
+
+    with st.form(key='photo_form'):
+        foto_ok = st.radio("¿Quieres guardar esta foto?", ("Sí", "No"))
+        submit_button_photo = st.form_submit_button(label='Confirmar')
+
+    if submit_button_photo and foto_ok == "No":
+        st.write("No se ha guardado la foto. Puedes tomar otra foto")
+    elif submit_button_photo and foto_ok == "Sí":
+        state.photo_form_submitted = True
+
+if state.photo_form_submitted:
+    with st.form(key='name_form'):
         name = st.text_input("Tu nombre, por favor:")
+        submit_button_name = st.form_submit_button(label='Confirmar')
+
+    if submit_button_name and name == "":
+        st.write("No has introducido tu nombre")
+    elif submit_button_name:
+        state.name_form_submitted = True
         file_name = f"{name}.jpg"
+        st.write(f"file_name: {file_name}")
         image_path = os.path.join(faces_folder, file_name)
 
-        # Read the image data from the picture variable
+        # Resto del código para guardar la imagen...
         picture_data = np.frombuffer(picture.read(), np.uint8)
-
-        # Decode the image data
         picture_img = cv2.imdecode(picture_data, cv2.IMREAD_COLOR)
-
-        # Save the image
         cv2.imwrite(image_path, picture_img)
 
-        st.write(f"{name} tu foto se ha guardado con tu nombre. Ya puedes ir a la pestaña de reconocimiento facial")
-    else:
-        st.write("No se ha guardado la foto. Puedes tomar otra foto")
+        st.write(f"{name}, gracias por registrarte")
